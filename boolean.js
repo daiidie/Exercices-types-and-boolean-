4e92d80c2b8ce08ab13d0ba1e3b817a8c1d5e282
daiidie <delavegae46@gmail.com>
2021-02-15 16:25:57 +0100

    fix

--- boolean.js
+++ boolean.js
@@ -8,18 +8,16 @@ let res3 = (true || false) && true
 console.log(`res3 = ${res3}`, res3)
 
 let res4 = (!true && !false) || (!false && !false)
-console.log(`res4 = ${res2}`, res2)
+console.log(`res4 = ${res4}`, res4)
 
 let res5 = 'Hello' === 'Hello' && 'World' == 'Word'
 console.log(`res5 = ${res5}`, res5)
-
 
 let res6 = (!(20 >= 20) && 7 === 7) || true
 console.log(`res6 = ${res6}`, res6)
 
 let res7 = '1' === 1 && '2' == 2 && '3' === 3
 console.log(`res7 = ${res7}`, res7)
-
 
 let res8 = !res7
 console.log(`res8 = ${res8}`, res8)
---
+++ trading.js
@@ -0,0 +1,16 @@
+let buy = 10
+let sell = ++10
+let hold >= 10  
+
+if (buy) {
+  hold += 'umbrella, ' // concatenation de string
+}
+
+if (temperature < 15) {
+  clothes += 'coat, '
+} else if (temperature >= 15 && temperature <= 20) {
+  clothes += 'sweater, '
+} else {
+  clothes += 't-shirt, '
+}
+console.log(`Je vous recommande de porter: ${clothes}`)
